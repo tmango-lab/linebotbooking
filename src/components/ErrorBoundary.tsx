ab9f1eea998c0aa05e22717cd9ba0e58f5351707
tmango-lab <tmango.fb@gmail.com>
2026-01-30 16:55:59 +0700

    feat: Add a React ErrorBoundary component to catch and display UI errors.

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,5 +1,6 @@
 
-import React, { Component, ErrorInfo, ReactNode } from 'react';
+import { Component } from 'react';
+import type { ErrorInfo, ReactNode } from 'react';
 
 interface Props {
     children?: ReactNode;
